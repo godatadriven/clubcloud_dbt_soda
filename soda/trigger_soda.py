23b9687065c4f75a9abc2456d1ea077ce819a233
guillesd <guillermo.sanchez@ah.nl>
2021-10-22 14:54:29 +0200

    finished programmatic trigger for soda

--- soda/trigger_soda.py
+++ soda/trigger_soda.py
@@ -1,24 +1,54 @@
 import argparse
+from pathlib import Path
 from sodasql.scan.scan_builder import ScanBuilder
 from typing import List
+from datetime import date
 
 def run_soda_scan(dataset: str, table: str):
     scan_builder = ScanBuilder()
-    scan_builder.warehouse_yml_file = "./datasets/{dataset}/{dataset}.yml"
-    scan_builder.scan_yml_file = "./datasets/{dataset}/tables/{table}.yml"
+    scan_builder.warehouse_yml_file = f"./datasets/{dataset}/{dataset}.yml"
+    scan_builder.scan_yml_file = f"./datasets/{dataset}/tables/{table}.yml"
+    scan_builder.variables = {"date": date.today().strftime("%d-%m-%Y")}
     scan = scan_builder.build()
     scan_result = scan.execute()
     print(f"Finished scan of {dataset}.{table} with {scan_result.get_test_failures_count()} errors")
 
 def run_partial_dataset_scan(dataset: str, tables: List[str]):
-    pass
+    print(f"Running a partial dataset scan on dataset {dataset}...")
+    for table in tables:
+        run_soda_scan(dataset, table)
 
 def run_full_dataset_scan(dataset: str):
-    pass
+    print(f"Running a full dataset scan on dataset {dataset}...")
+    path = Path(f"./datasets/{dataset}/tables/")
+    tables = [f.stem.split(',')[0] for f in path.glob("*.yml")]
+    for table in tables:
+        run_soda_scan(dataset, table)
 
 def run_full_warehouse_scan():
-    pass
+    print("Running a full warehouse scan...")
+    path = Path("./datasets/")
+    for p in path.iterdir():
+        run_full_dataset_scan(p.name)
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description="Helper to run progrmatic soda scans")
-    parser.add_argument("--dataset", type=str, help="Dataset containing the table that you want to scan")
+    parser.add_argument("--dataset", type=str, help="Dataset containing the table(s) that you want to scan", required=False)
+    parser.add_argument("--tables", type=str, help="Single table or several tables separated by ',' that you want scan", required=False)
+    args = parser.parse_args()
+
+    if args.dataset is None and args.tables is not None:
+        raise ValueError("You have provided table(s) but have not provided a dataset")
+
+    elif args.dataset is None and args.tables is None:
+        run_full_warehouse_scan()
+
+    elif args.dataset is not None and args.tables is None:
+        run_full_dataset_scan(args.dataset)
+
+    else:
+        if ',' in args.tables:
+            tables = args.tables.strip().split(',')
+            run_partial_dataset_scan(args.dataset, tables)
+        else:
+            run_soda_scan(args.dataset, args.tables)

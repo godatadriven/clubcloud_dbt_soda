c85012f86b806f649d60ea40cf3beae9382fd856
jgligorevic <jgligorevic@interfood.com>
2021-10-25 14:03:16 +0200

    Updated analytics and int_project_versions models

--- dbt/models/marts/analytics/analytics.sql
+++ dbt/models/marts/analytics/analytics.sql
@@ -3,18 +3,27 @@
          materialized='incremental',
          incremental_strategy = 'insert_overwrite',
          schema = 'core',
-         unique_key='version_id',
+         unique_key='project_id',
          partition_by={
-             "field": "version_created_timestamp",
+             "field": "project_created_timestamp",
              "data_type": "timestamp"
         }
      )
 }}
 {% set days_back = 7 %}
 
-with project_versions as(
-    select project_name,
+with latest_date as (
+    select
+            max(version_updated_timestamp) as latest_date
+    from    {{ ref('project_versions') }}
+
+
+),
+
+project_versions as(
+    select
            project_id,
+           project_name,
            project_description,
            repo_name,
            repo_owner,
@@ -23,8 +32,10 @@ with project_versions as(
            language,
            project_created_timestamp,
            count(*) as total_version_count,
-           sum(case when updated_timestamp >= date_sub(max(updated_timestamp), interval {{days_back}} day) then 1 else 0 end ) as releases_last_week
+           sum(case when DATE(version_updated_timestamp) >= date_sub(DATE(latest_date.latest_date), interval {{days_back}} day) then 1 else 0 end ) as releases_last_week
     from {{ ref('project_versions') }}
-
+    cross join latest_date
+    group by
+           1,2,3,4,5,6,7,8,9
 )
 select * from project_versions
--- dbt/models/marts/core/int_project_versions.sql
+++ dbt/models/marts/core/int_project_versions.sql
@@ -14,9 +14,14 @@
 
 with versions as (
 
-    select *,
+    select
            id as version_id,
-           created_timestamp as version_created_timestamp
+           project_id,
+           number,
+           published_timestamp as version_published_timestamp,
+           created_timestamp as version_created_timestamp,
+           updated_timestamp as version_updated_timestamp
+
     from {{ ref ('stg_versions') }}
 
 ),
@@ -24,7 +29,8 @@ with versions as (
 project_versions as (
 
     select versions.*,
-           project_repos.*
+           project_repos.* except (project_id),
+
     from versions
     left join {{ ref('project_repos') }} on project_repos.project_id = versions.project_id
 

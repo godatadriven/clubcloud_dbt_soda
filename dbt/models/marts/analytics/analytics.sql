8936cc973021a04ede6b7f10c5af8bfbe2bdad19
jgligorevic <jgligorevic@interfood.com>
2021-10-26 10:41:19 +0200

    Updated analytics model

--- dbt/models/marts/analytics/analytics.sql
+++ dbt/models/marts/analytics/analytics.sql
@@ -21,9 +21,9 @@ with project_versions as(
            language,
            project_created_timestamp,
            count(*) as total_version_count,
-           sum(case when DATE(version_updated_timestamp) >= date_sub(DATE({{ max_ingested_date }}), interval {{days_back}} day) then 1 else 0 end ) as releases_last_week
+           sum(case when DATE(version_updated_timestamp) >= date_sub(DATE('{{ max_ingested_date }}'), interval {{days_back}} day) then 1 else 0 end ) as releases_last_week
     from {{ ref('project_versions') }}
     group by
-           1,2,3,4,5,6,7,8,9
+           1,2,3,4,5,6,7,8
 )
 select * from project_versions
